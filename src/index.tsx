564c7ef5e83ba4a5c89bd726ca09b58ca4bef712
Andras Vagner <andras.vagner@gmail.com>
2023-10-02 18:26:04 +0200

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "~/components/App/App";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { theme } from "~/theme";
+import { worker } from "~/mocks/browser";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { refetchOnWindowFocus: false, retry: false, staleTime: Infinity },
+  },
+});
+
+worker.start({ onUnhandledRequest: "bypass" }).then(() => {
+  const container = document.getElementById("app");
+  const root = createRoot(container!);
+
+  root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <App />
+          </ThemeProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+});
